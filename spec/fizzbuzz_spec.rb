86de59495a134434e30209d5ffd4c2b15cecf17d
Britny Edwards <bredwards2@gmail.com>
2018-11-10 22:18:49 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,7 +9,7 @@ describe "fizzbuzz" do
     expect(fizz_3).to eq("Fizz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = buzz(5)
+    fizz_5 = fizzbuzz(5)
 
     expect(fizz_5).to eq("Buzz")
   end
