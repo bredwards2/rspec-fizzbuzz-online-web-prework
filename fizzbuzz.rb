af0d744a4304a67e1f80e9210a4bffa794ad92c8
Britny Edwards <bredwards2@gmail.com>
2018-11-10 22:04:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
- 
+def fizzbuzz(int)
+  if int % 3 == 0 && 
+    
+  elsif int % 3
+  puts ""
+  
+  else 
+    
+    
+  end
 end
+ 
