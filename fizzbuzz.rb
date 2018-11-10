70205388b5fcc52665769de5bfe4393a4e850378
Britny Edwards <bredwards2@gmail.com>
2018-11-10 22:07:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,10 @@ def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
   puts "Fizzbuzz"
   
-  elsif int % 3
+  elsif int % 3 == 0
   puts "Fizz"
   
-  else int % 5
+  else int % 5 == 0
   puts "Buzz"
     
   end
